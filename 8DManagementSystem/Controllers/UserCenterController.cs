61079c976ee25a737edfb438a9f2c46051dc8865
zhudongjie <zhudoney@aliyun.com>
2015-02-05 20:35:44 +0800

    测试

--- 8DManagementSystem/Controllers/UserCenterController.cs
+++ 8DManagementSystem/Controllers/UserCenterController.cs
@@ -26,8 +26,9 @@ namespace _8DManagementSystem.Controllers
             String jsondata = HttpUtility.UrlDecode(Request.Params[0], System.Text.Encoding.UTF8);
 
             int sEcho = 0;
-            string subCompanyId = string.Empty;
-            string picName = string.Empty;
+            string userName = string.Empty;
+            string userCode = string.Empty;
+            string departmentName = string.Empty;
             JArray jsonarray = Newtonsoft.Json.JsonConvert.DeserializeObject<JArray>(jsondata);
 
             for (int i = 0; i < jsonarray.Count; i++) //从传递参数里面选出待用的参数  
@@ -43,45 +44,48 @@ namespace _8DManagementSystem.Controllers
                     rowCount = jobj.Property("value").Value.EToInt();
 
                 if (jobj.Property("name").Value.ToString().Equals("SubCompany"))
-                    subCompanyId = jobj.Property("value").Value.EToString();
+                    userCode = jobj.Property("value").Value.EToString();
 
                 if (jobj.Property("name").Value.ToString().Equals("pictureName"))
-                    picName = jobj.Property("value").Value.EToString();
+                    userName = jobj.Property("value").Value.EToString();
             }
-
+            int totalCount = 0;
             //string subCompanyId = string.Empty;
-            IList<Model.D_User_Model> list = new DAL.D_User_DAL().GetAll();
-            int total = list.Count;
-            int totalPage = total % rowCount == 0 ? total / rowCount : total / rowCount + 1;
-            IList<object> objectList = new List<object>();
-            int count = 1;
-            foreach (var item in list.Skip(page).Take(rowCount))
-            {
-                //objectList.Add(new string[] { 
-                //    item.Id.ToString(), count.ToString(), EnumUtility.GetEnumDescription(item.SubCompanyId), item.PicType, item.PicName, item.PicPath, item.PicDirectId,  
-                //    item.IfValid == true ? "可用" : "不可用",item.Ord.ToString(),item.OpUserName,item.OpDate.HasValue ? item.OpDate.Value.ToString("yyyy-MM-dd HH:mm:ss") : "" });
-                //count++;
-            }
+            //IList<Model.D_User_Model> list = new DAL.D_User_DAL().GetAll();
+            IList<Model.D_User_Model> list = new DAL.D_User_DAL().GetAllByPage(page, rowCount, userName, userCode, departmentName, out totalCount);
+
+            int totalPage = totalCount % rowCount == 0 ? totalCount / rowCount : totalCount / rowCount + 1;
 
             ArrayList rows = new ArrayList();
-            foreach (var item in list.Skip(page).Take(rowCount))
+            foreach (var item in list)
             {
                 rows.Add(new
                 {
 
-                    //id = item.Id,
-//                    SubCompanyId = item.SubCompanyId,
-//                    SubCompanyName = EnumUtility.GetEnumDescription(item.SubCompanyId),
-//                    PicType = item.PicType,
-//                    PicName = item.PicName,
-//                    PicPath = string.Format(@" <img alt='{1}' src='{0}' style='width:90px;height:90px;' />", item.PicPath, item.PicName),
-//                    PicDirectId = item.PicDirectId,
-//                    IfValid = item.IfValid == true ? "可用" : "不可用",
-//                    Ord = item.Ord,
-//                    //OpUserName = item.OpUserName,
-//                    OpDate = item.OpDate.HasValue ? item.OpDate.Value.ToString("yyyy-MM-dd HH:mm:ss") : "",
-//                    Oper = string.Format(@"<div class='hidden-sm hidden-xs btn-group'><button class='btn btn-xs btn-info' id='{0}' onclick='edit(this.id)'><i class='ace-icon fa fa-pencil bigger-120'></i></button>
-//                                            <button class='btn btn-xs btn-danger' id='{0}' onclick='del(this.id)'><i class='ace-icon fa fa-trash-o bigger-120'></i></button></div>", item.Id.ToString())
+                    id = item.UserGuid,
+                    UserCode = item.UserCode,
+                    LoginName = item.UserLoginName,
+                    UserName = item.UserName,
+                    Password = "******",
+                    DepartmentName = item.DepartmentName,
+                    IsAdmin = item.IsAdmin ? "Yes" : "No",
+                    ModifyUserName = item.ModifyUserName,
+                    ModifyDate = item.ModifyDateTime.HasValue ? item.ModifyDateTime.Value.ToString() : string.Empty,
+                    CreateUserName = item.CreateUserName,
+                    CreateDate = item.CreateDateTime.HasValue ? item.CreateDateTime.Value.ToString("yyyy-MM-dd") : string.Empty
+
+                    //                    SubCompanyId = item.SubCompanyId,
+                    //                    SubCompanyName = EnumUtility.GetEnumDescription(item.SubCompanyId),
+                    //                    PicType = item.PicType,
+                    //                    PicName = item.PicName,
+                    //                    PicPath = string.Format(@" <img alt='{1}' src='{0}' style='width:90px;height:90px;' />", item.PicPath, item.PicName),
+                    //                    PicDirectId = item.PicDirectId,
+                    //                    IfValid = item.IfValid == true ? "可用" : "不可用",
+                    //                    Ord = item.Ord,
+                    //                    //OpUserName = item.OpUserName,
+                    //                    OpDate = item.OpDate.HasValue ? item.OpDate.Value.ToString("yyyy-MM-dd HH:mm:ss") : "",
+                    //                    Oper = string.Format(@"<div class='hidden-sm hidden-xs btn-group'><button class='btn btn-xs btn-info' id='{0}' onclick='edit(this.id)'><i class='ace-icon fa fa-pencil bigger-120'></i></button>
+                    //                                            <button class='btn btn-xs btn-danger' id='{0}' onclick='del(this.id)'><i class='ace-icon fa fa-trash-o bigger-120'></i></button></div>", item.Id.ToString())
 
 
                 });
@@ -89,7 +93,7 @@ namespace _8DManagementSystem.Controllers
 
             //return Json(new { sEcho = rowCount, iTotalRecords = total, iTotalDisplayRecords = total, aaData = rows }, JsonRequestBehavior.AllowGet);
 
-            return Json(new { sEcho = sEcho, iTotalRecords = total, iTotalDisplayRecords = total, aaData = rows }, JsonRequestBehavior.AllowGet);
+            return Json(new { sEcho = sEcho, iTotalRecords = totalCount, iTotalDisplayRecords = totalCount, aaData = rows }, JsonRequestBehavior.AllowGet);
 
 
         }
